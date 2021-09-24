ed8c0bb1f50d8f3b838efe717c3c73976bec5d22
Us10n <68775862+Us10n@users.noreply.github.com>
2021-09-24 19:13:53 +0300

    Update MenuPosition.kt

--- app/src/main/java/com/example/foodshop/recycler/MenuPosition.kt
+++ app/src/main/java/com/example/foodshop/recycler/MenuPosition.kt
@@ -1,8 +1,11 @@
 package com.example.foodshop.recycler
 
 data class MenuPosition(
-    var id: Long=0,
-    var name:String="None",
-    var imgUrl: String="https://firebasestorage.googleapis.com/v0/b/test-4c0c2.appspot.com/o/imgs%2Ftest4.png?alt=media&token=6d42e510-bcf0-4d1d-b52a-fb2a01eb5f83",
-    var description: String="None"){
+    var id: Long = 0,
+    var name: String = "None",
+    var imgUrl: String = "https://firebasestorage.googleapis.com/v0/b/test-4c0c2.appspot.com/o/imgs%2Ficon.jpg?alt=media&token=ab861299-bea9-4cad-b803-30d731e9afe9",
+    var description: String = "None",
+    var price: String = "0",
+    var type: String = "None"
+) {
 }
